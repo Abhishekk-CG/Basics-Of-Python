5b9cdad4ad8bd0f342472a33a55a261b2030005c
ABHISHEK KUMAR <abhishekk.cg@gmail.com>
2026-08-23 15:54:28 +0530

    about_python.py file added

--- 01_intro to python/about_python.py
+++ 01_intro to python/about_python.py
@@ -1,2 +1,2 @@
 print("learning python is the most important thing in todays world")
-#python is a high level interpreter based programming language.
+#python is a high level interpreter based programming language.    
