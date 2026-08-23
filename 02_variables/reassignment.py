120c5b5528a96f5c557af2c54cff9ae2581908ab
ABHISHEK KUMAR <abhishekk.cg@gmail.com>
2026-08-23 16:33:53 +0530

    assignment completed

--- 02_variables/reassignment.py
+++ 02_variables/reassignment.py
@@ -1,4 +1,19 @@
+#26
 student_name = "rahul"
 marks = 91
 marks = 95
 print(student_name,marks)
+#27
+name = "Rahul"
+age = 18
+city = "Patna"
+print(name,age,city)
+#28
+x=y=z=0
+print(x,y,z)
+#30
+#student all information 
+students_name = "Abhishek"
+Students_age = 18
+Students_age = 19
+print(students_name,Students_age)
