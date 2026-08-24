f4174fe7daa5ec90102cb41b70ff2098cc9a4b2a
ABHISHEK KUMAR <abhishekk.cg@gmail.com>
2026-08-24 17:53:56 +0530

    datatypes assignment done

---
+++ 03_datatypes/assignment.py
@@ -0,0 +1,68 @@
+#23
+a = 10
+b = 10.0
+c = "10"
+d = True
+e = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+#24
+student_name = "Abhishek"
+student_age = 18
+student_height = 172.0
+person_is_a_student = True
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(person_is_a_student))
+#25
+ab = 50
+bc = 50.0
+cd = "50"
+print(type(ab))
+print(type(bc))
+print(type(cd))
+#26
+value = True
+Value = "True"
+print(type(value))
+print(type(Value))
+#27
+abc=None
+bcd="None"
+print(type(abc))
+print(type(bcd))
+#28
+valua=25
+print(type(valua))
+valua="25"
+print(type(valua))
+#29
+product_name = "apple"
+product_quantity = 1
+product_price = 49999.99
+whether_the_product_is_available = True
+discount = None
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(whether_the_product_is_available))
+print(type(discount))
+#30
+value1 = 10
+value2 = 10.0
+value3 = "10"
+value4 = True
+value5 = "True"
+value6 = None
+value7 = "None"
+print(type(value1))
+print(type(value2))
+print(type(value3))
+print(type(value4))
+print(type(value5))
+print(type(value6))
+print(type(value7))
