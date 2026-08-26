d0576186d10e9480bec907359dd123ebd9b38841
ABHISHEK KUMAR <abhishekk.cg@gmail.com>
2026-08-26 10:04:13 +0530

    4th assignment added

---
+++ 04_arithmetic operators/assignments.py
@@ -0,0 +1,126 @@
+#1
+a=10
+b=-5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+#2
+c=10
+d=5.0
+print(c+d)
+print(c-d)
+print(c*d)
+print(c/d)
+print(c//d)
+print(c**d)
+print(c%d)
+#3
+phy=95
+che=90
+bio=94
+total_marks = (phy+che+bio)
+average_marks = (phy+che+bio)/3
+print(total_marks)
+print(average_marks)
+#4
+product_price=500
+quantity=3
+total_price=(product_price*quantity)
+print(total_price)
+#5
+e=int(input("enter a number"))
+if e%2 == 0 :
+ print("the number is even")
+else:
+ print("the number is odd")
+ #6
+f=int(input("enter a number"))
+g=int(input("enter a number"))
+normal_division=f/g
+floor_division=f//g
+print("normal division =",normal_division)
+print("floor division =",floor_division)
+#7
+h=-10
+i=-25
+print(h+i)
+print(h-i)
+print(h*i)
+print(h/i)
+print(h//i)
+print(h**i)
+print(h%i)
+#8
+positive=5
+negative=-5
+print(positive - positive)
+print(positive - negative)
+print(negative - positive)
+print(negative - negative)
+#9
+print(positive // positive)
+print(negative // positive)
+print(positive // negative)
+print(negative // negative)
+#10
+print(positive % positive)
+print(negative % positive)
+print(positive % negative)
+print(negative % negative)
+#11
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+#12
+
+
+#13
+j=True
+k=False
+print(j+k)
+print(j-k)
+print(j*k)
+print(j/k)
+print(j//k)
+print(j**k)
+print(j%k)
+#15
+first_name="Abhishek"
+last_name="Thakur"
+print(first_name+last_name)
+#16
+l=2.0
+print(first_name*3)
+print(first_name*l)
+#17
+print(l+l)
+print(l-l)
+print(l*l)
+print(l/l)
+#20
+m=23
+n=2
+add=m+n
+sub=m-n
+mul=m*n
+div=m/n
+f_div=m//n
+mod=m%n
+exp=m**n
+print("addition=",add)
+print("subtraction=",sub)
+print("multiplication=",mul)
+print("division=",div)
+print("floor division=",f_div)
+print("modulus=",mod)
+print("exponential=",exp)
+#21
+o = 10
+p = -3
+q = 2.5
