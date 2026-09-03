4a7df1a5d581601ff2dc8bda78c5c695e8b7c2ac
ABHISHEK KUMAR <abhishekk.cg@gmail.com>
2026-09-03 09:41:22 +0530

    till 5th assignment done

---
+++ 05_string operations/assignment.py
@@ -0,0 +1,109 @@
+#1.
+your_name = "John Doe"
+your_city = "New York"
+fav_language = 'Python'
+message = "This is me writing this code."
+print(your_name,your_city,fav_language,message)
+#2
+empty = ("")
+print(empty)
+print(len(empty))
+print(type(empty))
+#3.
+third = "Python Programming"
+print(third)
+print(len(third))
+print(third[0])
+print(third[-1])
+print(third[2])
+print(third[-2])
+#4.
+forth = "Programming"
+print(forth[0])
+print(forth[1])
+print(forth[4])
+print(forth[10])
+#5.
+print(forth[-1])
+print(forth[-2])
+print(forth[-3])
+print(forth[-11])
+#6.
+sixth = "Abhishek Thakur"
+print(sixth[0])
+print(sixth[-1])
+print(sixth[9])
+#7.
+seventh = "Python Programming"
+print(seventh[0:6])
+print(seventh[7:18])
+print(seventh[0:5])
+print(seventh[13::1])
+#8.
+eighth = "ABCDEFGHIJKL"
+print(eighth[::2])
+print(eighth[::3])
+print(eighth[1:9:2])
+print(eighth[::-1])
+#9.
+ninth = "Python Programming"
+print(ninth[13::1])
+print(ninth[8::1])
+print(ninth[::-1])
+#10.
+tenth = "ABCDEFGHIJ"
+print(tenth[:4:1])
+print(tenth[8::1])
+print(tenth[::2])
+print(tenth[::-1])
+print(tenth[1:10:1])
+#11.
+eleventh = "apple"
+eleventh1 = "this's apple"
+eleventh2 = "this is apple and this is orange"
+print(len(eleventh))
+print(len(eleventh1))
+print(len(eleventh2))
+#12.
+twelfth = "Python Programming"
+size = len(twelfth)
+print(twelfth[size - 1])
+#13.
+first_name="Abhishek"
+last_name="Thakur"
+full_name=first_name + " " + last_name
+print(full_name)
+#14.
+Name = "Abhishek Thakur"
+Age = 25
+City = "New York"
+Programming_language = "Python"
+print("My name is {Name}. I am {Age} years old. I live in {City} and my favorite programming language is {Programming_language}.".format(Name=Name, Age=Age, City=City, Programming_language=Programming_language))
+#15.
+name="Abhishek"
+age=20
+c=name+str(age)
+print(c)
+#16
+d="@abxii07"
+print(d*3)
+print(d*5)
+print(d*10)
+#17.
+e="*"
+print(e*10)
+#18.
+f="python programming language"
+print(f.upper())
+print(f.lower())
+print(f.capitalize())
+print(f.title())
+print(f.swapcase())
+#19.
+g="Python"
+h="python"
+i=(g==h)
+j=g.lower()==h.lower()
+print(i)
+print(j)
+#20.
